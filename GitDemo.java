edf4c00e7783f4e079a673da80dd002baea0ee8e
ThuwaibaAdam <thuwaiba.dow11@gmail.com>
2024-02-14 10:43:53 -0500

    this is Asian TimeZone

---
+++ GitDemo.java
@@ -0,0 +1,12 @@
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is me trying to learn GitHub");
+		System.out.println("This need disaplain and hard work to grassp");
+		
+	}
+
+}
